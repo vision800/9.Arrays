7371e34232f62cad8b77678b25510c60bd182f6e
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:01:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring and initializing an array
+            int[] numbers = { 1, 2, 3, 4, 5, };
+            Console.WriteLine(numbers[3]);
+
+            string[] subjects = new string[3];
+            subjects[0] = "Mathematics";
+            subjects[1] = "Life sciences";
+            subjects[2] = "Peography";
+            Console.WriteLine(numbers[2]);
+            Console.ReadLine();
+
+        }
+    }
+}
